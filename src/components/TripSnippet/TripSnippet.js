a92d8a2d04755c2dd0e1e8ad3eb179214751afc5
Matt Hendrick <75456865+matt-hendrick@users.noreply.github.com>
2021-01-09 11:29:42 -0500

    Deleted ProfileCard. Cleaned  TripSnippet/Skeleton

--- src/components/TripSnippet/TripSnippet.js
+++ src/components/TripSnippet/TripSnippet.js
@@ -1,8 +1,27 @@
 import React from 'react';
-
+import { makeStyles } from '@material-ui/core/styles';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 import PropTypes from 'prop-types';
 
-import ProfileCard from '../ProfileCard/ProfileCard';
+import { Link } from 'react-router-dom';
+
+// MUI Stuff
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles(() => ({
+  card: {
+    position: 'relative',
+    display: 'flex',
+    marginBottom: 20,
+  },
+  content: {
+    padding: 25,
+    objectFit: 'cover',
+  },
+}));
 
 function TripSnippet(props) {
   const {
@@ -15,13 +34,29 @@ function TripSnippet(props) {
     },
   } = props;
 
+  const classes = useStyles();
+
+  dayjs.extend(relativeTime);
+
   return (
-    <ProfileCard
-      name={tripName}
-      link={`/trips/${tripID}`}
-      createdAt={createdAt}
-      members={members}
-    />
+    <Card className={classes.card}>
+      <CardContent className={classes.content}>
+        <Typography
+          variant="h6"
+          component={Link}
+          to={`/trips/${tripID}`}
+          color="primary"
+        >
+          {tripName}
+        </Typography>
+        <Typography variant="body2" color="textSecondary">
+          Created {dayjs(createdAt).fromNow()}
+        </Typography>
+        {members ? (
+          <Typography variant="body1">Members: {members.join(', ')}</Typography>
+        ) : null}
+      </CardContent>
+    </Card>
   );
 }
 
